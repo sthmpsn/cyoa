77ace0fc2fc6c0c4a2de6425a32aae525148f35c
Scott Anderson <Scott.T.Anderson50@gmail.com>
2019-03-30 11:45:14 -0500

    fixed pathing

--- models/schema.sql
+++ models/schema.sql
@@ -1,3 +1,12 @@
 DROP DATABASE IF EXISTS cyoa;
 CREATE DATABASE cyoa;
+USE cyoa;
 
+CREATE TABLE users (
+    ID int,
+    username varchar(255),
+    password varchar(255),
+    score int,
+    stress int,
+    currentQuestionId int, 
+)
