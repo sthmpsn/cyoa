7cabca7d2942a57776251a3e731d8bfe27d0b79e
sthmpsn <sthmpsn@gmail.com>
2019-03-31 01:45:45 -0500

    added scoreboard route and logic to display all users in Rank order and styled 1st 2nd 3rd appropriately

--- public/js/scoreboard.js
+++ public/js/scoreboard.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
   console.log("Scoreboard JS Loaded");
+  var rank = 0;
+
+  // Get the RANK, NAME, and SCORE elements by their ID
+  var $sbRankDiv = $("#scoreboard-rank");
+  var $sbNameDiv = $("#scoreboard-name");
+  var $sbScoreDiv = $("#scoreboard-score");
+
   // GET a request to the /scoreboard route and return the list of users in rank of highest to lowest score
   // Limit to top 10 users
   $.ajax({
@@ -7,10 +14,35 @@ $(document).ready(function() {
     url: "/api/scoreboard"
   }).then(function(result) {
     console.log("GET Scoreboard users running");
+
     result.forEach(function(user) {
+      ++rank;
+      console.log("Rank: " + rank);
       console.log(user.username);
       console.log(user.score);
       console.log(user.stress);
+
+      var rankEl = $("<p class=\"h3 my-4\">" + rank + "</p>");
+      var nameEl = $("<p class=\"h3 my-4\">" + user.username + "</p>");
+      var scoreEl = $("<p class=\"h3 my-4\">" + user.score + "</p>");
+
+      if (rank === 1) {
+        rankEl.addClass("gold");
+        nameEl.addClass("gold");
+        scoreEl.addClass("gold");
+      } else if (rank === 2) {
+        rankEl.addClass("silver");
+        nameEl.addClass("silver");
+        scoreEl.addClass("silver");
+      } else if (rank === 3) {
+        rankEl.addClass("bronze");
+        nameEl.addClass("bronze");
+        scoreEl.addClass("bronze");
+      }
+
+      $sbRankDiv.append(rankEl);
+      $sbNameDiv.append(nameEl);
+      $sbScoreDiv.append(scoreEl);
     });
   });
 });
